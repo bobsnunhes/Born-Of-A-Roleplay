728b50fc3af7b3bfb6a8b5134c7e5cb3f5712c44
bobsnunhes <bobsnunhes@hotmail.com>
2019-04-18 13:41:59 +0000

    Adicionada tradução para entregador de pizzas

--- resources/[sesx]/esx_pizza/__resource.lua
+++ resources/[sesx]/esx_pizza/__resource.lua
@@ -2,7 +2,8 @@ server_scripts {
     '@es_extended/locale.lua',
 	'locales/de.lua',
 	'locales/en.lua',
-    'locales/fr.lua',
+	'locales/fr.lua',
+	'locales/br.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -11,7 +12,8 @@ client_scripts {
 	'@es_extended/locale.lua',
 	'locales/de.lua',
 	'locales/en.lua',
-    'locales/fr.lua',
+	'locales/fr.lua',
+	'locales/br.lua',
 	'config.lua',
 	'client/main.lua'
 }
