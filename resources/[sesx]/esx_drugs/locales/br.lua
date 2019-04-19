120b0c152cba2148c22e9ec1c267d7932d9c2542
bobsnunhes <bobsnunhes@hotmail.com>
2019-04-19 15:38:55 +0000

    Adicionado ESX_DRUGS - Para venda de maconha

---
+++ resources/[sesx]/esx_drugs/locales/br.lua
@@ -0,0 +1,23 @@
+Locales ['br'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'Pressione ~INPUT_CONTEXT~ para colher o ~g~Pé de Maconha~s~.',
+  ['weed_inventoryfull'] = 'Você não tem espaço no inventário para mais ~g~Pés de Maconha~s~.',
+  ['weed_processprompt'] = 'Pressione ~INPUT_CONTEXT~ para iniciar o ~g~Processamento do Pé de Maconha~s~.',
+  ['weed_processingstarted'] = 'Processando o ~g~pé de maconha~s~ em ~g~Maconha~s~...',
+  ['weed_processingfull'] = 'Processamento ~r~cancelado~s~: Inventário cheio!',
+  ['weed_processingenough'] = 'Você deve ter no mínimo ~b~3x~s~ ~g~Pés de Maconha~s~ para processar.',
+  ['weed_processed'] = 'Você processou ~b~3x~s~ ~g~pés de maconha~s~ em ~b~1x~s~ ~g~Maconha~s~',
+  ['weed_processingtoofar'] = 'O processamento foi ~r~cancelado~s~ por abandonar a área.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'Pressione ~INPUT_CONTEXT~ para falar com o ~r~Traficante~s~.',
+  ['dealer_title'] = 'Traficante',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'Você não tem o suficiente para realizar a venda!',
+  ['dealer_sold'] = 'Você vendeu ~b~%sx~s~ ~y~%s~s~ por ~g~$%s~s~',
+
+  -- blips
+  ['blip_weedfield'] = 'Plantação de Maconha',
+  ['blip_weedprocessing'] = 'Processamento de Maconha',
+  ['blip_drugdealer'] = 'Traficante',
+}
