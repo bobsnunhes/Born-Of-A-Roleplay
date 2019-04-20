c9ee58873eef0728f07fcf9806a18edca014c043
bobsnunhes <bobsnunhes@hotmail.com>
2019-04-20 00:45:24 +0000

    adicionada loja de armas

---
+++ resources/[sesx]/esx_weaponshop/locales/br.lua
@@ -0,0 +1,13 @@
+Locales ['br'] = {
+  ['buy_license'] = 'Comprar licensa de armas?',
+  ['yes'] = '%s',
+  ['no'] = 'Não',
+  ['buy'] = 'você comprou ~y~%s~s~ for ~r~R$%s~s~',
+  ['not_enough_black'] = 'você não tem dinheiro sujo suficiente',
+  ['not_enough'] = 'você não tem dinheiro suficiente',
+  ['already_owned'] = 'você já tem esta arma!',
+  ['shop'] = 'comprar',
+  ['shop_menu'] = 'pressione ~INPUT_CONTEXT~ para comprar armas.',
+  ['shop_menu_item'] = '$%s',
+  ['map_blip'] = 'loja de Armas',
+}
