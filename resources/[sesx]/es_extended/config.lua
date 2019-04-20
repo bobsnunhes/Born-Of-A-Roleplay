5b75ab96af92e47e5982d7e379b1a1b414416368
bobsnunhes <bobsnunhes@hotmail.com>
2019-04-20 20:50:09 +0000

    Habilitado hud padrao para dinheiro do banco, e dinheiro sujo

--- resources/[sesx]/es_extended/config.lua
+++ resources/[sesx]/es_extended/config.lua
@@ -7,7 +7,7 @@ Config.AccountLabels        = { bank = _U('bank'), black_money = _U('black_money
 Config.EnableSocietyPayouts = false -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.ShowDotAbovePlayer   = false
 Config.DisableWantedLevel   = true
-Config.EnableHud            = false -- Alterado HUD de dinheiro e emprego
+Config.EnableHud            = true -- Roberto - Alterado HUD de dinheiro e emprego
 
 Config.PaycheckInterval     = 7 * 60000
 Config.MaxPlayers           = GetConvarInt('sv_maxclients', 32)
