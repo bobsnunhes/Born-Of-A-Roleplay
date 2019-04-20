7e696eca700fac62c0acec69be767ca1e5371531
bobsnunhes <bobsnunhes@hotmail.com>
2019-04-20 00:59:50 +0000

    adicionado rpchat

---
+++ resources/[sesx]/esx_rpchat/locales/br.lua
@@ -0,0 +1,12 @@
+Locales['br'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'Enviar um tweet',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'Ação pessoal',
+  ['me_prefix'] = 'eu | %s',
+  ['do_help'] = 'Informação do RP',
+  ['do_prefix'] = 'de | %s',
+  ['generic_argument_name'] = 'mensagem',
+  ['generic_argument_help'] = 'a mensagem',
+  ['unknown_command'] = '^3%s^0 não é um comando válido!',
+}
