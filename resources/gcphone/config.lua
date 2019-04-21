5ac730ab4602e0ea6fc8b674f9b0cb2fbbd80895
bobsnunhes <bobsnunhes@hotmail.com>
2019-04-21 02:33:09 +0000

    adicionado GCPHONE

---
+++ resources/gcphone/config.lua
@@ -0,0 +1,9 @@
+FixePhone = {
+  
+  ['190'] = { name =  "Polícia", coords = { x = 441.2, y = -979.7, z = 30.58 } },
+  
+  -- Cabine telefonica proximo a policia
+  ['008-0001'] = { name = "Cabine Telephonique - Policia", coords = { x = 372.25, y = -965.75, z = 28.58 } },
+}
+
+ShowNumberNotification = false -- Show Number or Contact Name when you receive new SMS
