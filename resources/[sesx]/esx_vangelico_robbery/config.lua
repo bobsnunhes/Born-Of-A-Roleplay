cbfef14956de0247325821ba35ca46bfca3aa167
bobsnunhes <bobsnunhes@hotmail.com>
2019-04-26 03:27:44 +0000

    roubo a joalheria adicionado

---
+++ resources/[sesx]/esx_vangelico_robbery/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+Config.Locale = 'br'
+
+Config.RequiredCopsRob = 0
+Config.RequiredCopsSell = 0
+
+Stores = {
+	["jewelry"] = {
+		position = { ['x'] = -629.99, ['y'] = -236.542, ['z'] = 38.05 },       
+		reward = math.random(100000,150000),
+		nameofstore = "jewelry",
+		lastrobbed = 0
+	}
+}
